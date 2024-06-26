1d48ca0e30dd88c921c68349ac692f6e387901f2
irunatbullets <murray.nuttall@gmail.com>
2024-06-26 11:25:20 +0200

    Add unocss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@unocss/nuxt'
+  ],
+  css: [
+    '@unocss/reset/tailwind.css',
+    '~/assets/css/main.css',
+  ],
+  app: {
+    head: {
+      title: 'irunatbullets',
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1'
+    }
+  }
 })
---
+++ uno.config.ts
@@ -0,0 +1,33 @@
+// uno.config.ts
+import {
+  defineConfig, presetAttributify, presetIcons,
+  presetTypography, presetWebFonts,
+  transformerVariantGroup
+} from 'unocss'
+
+import presetWind from '@unocss/preset-wind'
+
+export default defineConfig({
+  shortcuts: [
+    // ...
+  ],
+  theme: {
+    colors: {
+      // ...
+    }
+  },
+  presets: [
+    presetWind(),
+    presetAttributify(),
+    presetIcons(),
+    presetTypography(),
+    presetWebFonts({
+      fonts: {
+        Inter: 'Inter:400,500,600,700,800,900'
+      },
+    }),
+  ],
+  transformers: [
+    transformerVariantGroup(),
+  ],
+})
