c3d48d22b3194210a2fbe30b485ecd128abd7131
irunatbullets <murray.nuttall@gmail.com>
2024-06-26 12:04:26 +0200

    Fix favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,7 +13,8 @@ export default defineNuxtConfig({
     head: {
       title: 'irunatbullets',
       charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1'
+      viewport: 'width=device-width, initial-scale=1',
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
     }
   }
 })
