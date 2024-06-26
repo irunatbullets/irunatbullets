e770581c9c39ef9587c9f5c8719203f8857b9d67
irunatbullets <murray.nuttall@gmail.com>
2024-06-26 14:49:13 +0200

    Added prettier for tailwind sorting

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,12 @@
 <template>
-  <main class="container mx-auto px-4 sm:px-8">
-    <NuxtPage />
-  </main>
+  <div class="flex min-h-screen flex-col">
+    <header class="mb-4 bg-slate-500">
+      <div class="container mx-auto p-4 sm:p-8">
+        <h1 class="font-bold text-white">irunatbullets</h1>
+      </div>
+    </header>
+    <main class="container mx-auto flex-1 px-4 sm:px-8">
+      <NuxtPage />
+    </main>
+  </div>
 </template>
